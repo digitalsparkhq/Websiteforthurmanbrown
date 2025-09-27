c234fc664a36eaa692cb1153afa40aa6b07452f2
digitalsparkhq <biswajit@digitalsparkhq.com>
2025-09-27 20:16:47 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,38 @@
+// Navbar scroll effect
+window.addEventListener("scroll", function () {
+  const navbar = document.getElementById("navbar");
+  if (window.scrollY > 50) {
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
+});
 
+// Counter animation
+const counters = document.querySelectorAll(".counter");
+const speed = 200;
+
+const animateCounters = () => {
+  counters.forEach(counter => {
+    const updateCount = () => {
+      const target = +counter.getAttribute("data-target");
+      const count = +counter.innerText;
+      const increment = target / speed;
+
+      if (count < target) {
+        counter.innerText = Math.ceil(count + increment);
+        setTimeout(updateCount, 20);
+      } else {
+        counter.innerText = target;
+      }
+    };
+    updateCount();
+  });
+};
+
+window.addEventListener("scroll", () => {
+  const statsSection = document.querySelector(".stats");
+  if (statsSection && statsSection.getBoundingClientRect().top < window.innerHeight) {
+    animateCounters();
+  }
+}, { once: true });
